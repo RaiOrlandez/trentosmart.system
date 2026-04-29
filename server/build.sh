5babd73577e7c375308a9bc329779d02a88514e1
Rai <ryanmorlandez@asscat.edu.ph>
2026-04-29 16:41:57 +0800

    Auto-seed admin account during Railway build

--- server/build.sh
+++ server/build.sh
@@ -6,3 +6,6 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+# Automatically create an Admin account during build so the user doesn't have to!
+python manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(username='admin').exists() or User.objects.create_superuser('admin', 'admin@transmart.com', 'admin123')"
